108ea0b45abaddd8078173d8d28a17c9e5d4d1ca
Joel Garbers <joel.garbers@gmail.com>
2023-11-21 20:32:41 +0100

    Vi målade en ruta i skärmen som skapar en blå rektangel som är jobbig

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import time
 import pyautogui
 from PIL import Image, ImageDraw
+import tkinter as tk
 
-MARGIN = 200
+MARGIN = 30
 
 def get_mouse_position():
     # Get the mouse position
@@ -19,11 +20,24 @@ def get_screen_image():
 
 
 def dim_and_highlight(mouse_x, mouse_y, image):
-    overlay = Image.new("RGBA", image.size, (0, 0, 0, 0))
-    draw = ImageDraw.Draw(overlay)
-    draw.rectangle([(mouse_x-MARGIN, mouse_y-MARGIN), (mouse_x+MARGIN, mouse_y+MARGIN)], fill=(255, 0, 0, 128))  # Red rectangle with alpha
-    screenshot = Image.alpha_composite(image.convert("RGBA"), overlay)
-    screenshot.show()
+    #draw.rectangle([(mouse_x-MARGIN, mouse_y-MARGIN), (mouse_x+MARGIN, mouse_y+MARGIN)], fill=(255, 0, 0, 128))  # Red rectangle with alpha
+    root = tk.Tk()
+    root.overrideredirect(True)
+    root.geometry("+0+0")
+    root.lift()
+    root.wm_attributes("-topmost", True)
+    root.wm_attributes("-disabled", True)
+    root.wm_attributes("-transparentcolor", "white")
+
+    canvas = tk.Canvas(root, bg='white', width=2560, height=1440)
+    
+    canvas.create_rectangle(mouse_x-MARGIN, mouse_y-MARGIN, mouse_x+MARGIN, mouse_y+MARGIN, outline='black', fill='blue')
+    canvas["state"] = "disabled"
+    canvas.pack()
+        
+    
+    
+    #root.mainloop()
 
 def main():
     mouse_x, mouse_y = get_mouse_position()
@@ -33,4 +47,5 @@ def main():
     dim_and_highlight(mouse_x, mouse_y, image)
 
 if __name__ == "__main__":
+    
     main()
